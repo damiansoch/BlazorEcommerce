790be3c46d01acf96cc8944bb8560406f5baa482
damiansoch <66294165+damiansoch@users.noreply.github.com>
2023-04-28 17:05:44 +0100

    dapper set uip

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -8,7 +8,7 @@ namespace BlazorEcommerce.Shared
 {
     public class Product
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
